94aab95d9d706e3674c53cef0dfec4af58dfd7be
AnaghaP1999 <142619903+Anaghap04@users.noreply.github.com>
2023-08-19 13:22:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');                                           
+const app = express();  
+const path = require('path'); 
+const morgan = require('morgan');
+const cors = require('cors');
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const PORT = 3000; 
+
+const db = require('./db/index');
+const api=require('./routers/router');
+app.use('/api',api);
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname +
+    '/dist/index.html')) });
+    
+app.listen(PORT,()=>{                                                         
+    console.log(`Server is running on ${PORT}`);                             
+})
