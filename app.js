ac6419428e38471aa2bb14ae7eea48b14c5e92ab
AnaghaP1999 <142619903+Anaghap04@users.noreply.github.com>
2023-08-19 13:53:19 +0530

    changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('./dist')); 
+app.use(express.static('./dist/todoapp')); 
 const PORT = 3000; 
 
 const db = require('./db/index');
